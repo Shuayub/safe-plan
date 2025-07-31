77b65ed8702a72d736b8f7e93232a6db5033dbe2
SaabirHussain <anwarsaabir@gmail.com>
2025-07-31 13:27:50 -0400

    Adding Support Screen

--- app/src/main/java/com/example/weatherapp/FirstFragment.java
+++ app/src/main/java/com/example/weatherapp/FirstFragment.java
@@ -31,7 +31,11 @@ public class FirstFragment extends Fragment {
 
         binding.buttonFirst.setOnClickListener(v ->
                 NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SupportFragment)//Change to supportfrag
+                        .navigate(R.id.action_FirstFragment_to_SupportFragment)
+        );
+        binding.btnReminder.setOnClickListener(v ->
+                NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_ReminderFragment)
         );
     }
 
